0afc901fdcaa7bfd1684073a938daa4a0c11d68f
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2020-06-12 23:48:03 +0530

    Every conference data accessed via API

--- front-end/conferences/src/App.js
+++ front-end/conferences/src/App.js
@@ -3,14 +3,14 @@ import React, {Component} from 'react';
 class App extends Component {
     state = {
         loading: true,
-        conference: null,
+        conferences: []
     };
 
     async componentDidMount(){
         const url = "https://o136z8hk40.execute-api.us-east-1.amazonaws.com/dev/get-list-of-conferences";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({conference: data.paid[0], loading:false});
+        this.setState({conferences: data.paid, loading:false});
     }
 
     render() {
@@ -18,22 +18,24 @@ class App extends Component {
             return <div>loading...</div>;
         }
 
-        if (!this.state.conference) {
+        if (!this.state.conferences.length) {
             return <div>No conference data available</div>;
         }
 
-        return (
-            <div>
-                <div>{this.state.conference.confName}</div>
-                <div>{this.state.conference.venue}</div>
-                <div>From : {this.state.conference.confStartDate}</div>
-                <div>To : {this.state.conference.confEndDate}</div>
-                <div>{this.state.conference.confUrl}</div>
-                <div>Register at - {this.state.conference.confRegUrl}</div>
-                <div>{this.state.conference.entryType}</div>
-                <img src={this.state.conference.imageURL} />
+        const conferencesJSX = this.state.conferences.map(conference => (
+            <div key={conference.conference_id}>
+                <div>{conference.confName}</div>
+                <div>{conference.venue}</div>
+                <div>From : {conference.confStartDate}</div>
+                <div>To : {conference.confEndDate}</div>
+                <div>{conference.confUrl}</div>
+                <div>Register at - {conference.confRegUrl}</div>
+                <div>{conference.entryType}</div>
+                <img src={conference.imageURL} />
             </div>
-        )
+        ));
+
+        return <div>{conferencesJSX}</div>
     }
 }
 
